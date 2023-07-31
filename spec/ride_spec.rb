f8e10de7fb27d664219bdc06f9a773244494a304
Hayden McSorley <haymcsorley@gmail.com>
2023-07-31 12:52:35 -0500

    Final commit

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -4,7 +4,7 @@ require_relative '../lib/visitor'
 RSpec.describe Ride do
   describe '#initialize' do
     it 'creates a ride with name, minimum height, admission fee, and excitement level' do
-      ride = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+      ride = Ride.new({name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle})
       expect(ride.name).to eq('Carousel')
       expect(ride.min_height).to eq(24)
       expect(ride.admission_fee).to eq(1)
@@ -12,21 +12,21 @@ RSpec.describe Ride do
     end
 
     it 'creates a ride with an empty rider log' do
-      ride = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+      ride = Ride.new({name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle})
       expect(ride.rider_log).to eq({})
     end
   end
 
   describe '#board_rider' do
     it 'boards a rider and adds them to the rider log' do
-      ride = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+      ride = Ride.new({name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle})
       visitor1 = Visitor.new('Bruce', 54, '$10')
       ride.board_rider(visitor1)
       expect(ride.rider_log).to eq({ visitor1 => 1 })
     end
 
     it 'does not board a rider who is not eligible' do
-      ride = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+      ride = Ride.new({name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle})
       visitor1 = Visitor.new('Bruce', 20, '$10')
       ride.board_rider(visitor1)
       expect(ride.rider_log).to eq({})
